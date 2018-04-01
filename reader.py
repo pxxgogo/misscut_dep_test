0799712726bb11245bafc48ec1c356c874d3c57d
潘星宇 <421915293@163.com>
2018-04-01 11:18:17 +0800

    init(add reader.txt)

---
+++ reader.py
@@ -0,0 +1,56 @@
+import re
+
+POSITION_COMPILER = re.compile("\[(\d+),(\d+)\]")
+
+class Reader:
+    def __init__(self, file_path):
+        with open(file_path) as file_handle:
+            raw_datas = file_handle.readlines()
+            self.parse_data(raw_datas)
+
+    def parse_data(self, raw_datas):
+        self.__datas = []
+        for line in raw_datas:
+            items = line.strip().split("\t")
+            if len(items) < 7:
+                continue
+            wrong_sentence = items[0]
+            mistake_type = items[1]
+            wrong_word = items[2]
+            correct_word = items[4]
+            word_position_str = POSITION_COMPILER.findall(items[5])[0]
+            word_position = (int(word_position_str[0]), int(word_position_str[1]))
+            mistake_flag = items[6]
+            if mistake_flag == 'False':
+                continue
+            correct_sentence = wrong_sentence[:word_position[0]] + correct_word + wrong_sentence[word_position[1]:]
+            data = {'correct_sentence': correct_sentence,
+                    'wrong_sentence:': wrong_sentence,
+                    'mistake_type': mistake_type,
+                    'wrong_word': wrong_word,
+                    'correct_word': correct_word,
+                    'word_position': word_position}
+            self.__datas.append(data)
+
+    def __call__(self):
+        for data in self.__datas:
+            yield data
+
+
+if __name__ == "__main__":
+    reader = Reader("mini_test_corpus.txt")
+    for data in reader():
+        print(data)
+        input()
+
+
+
+
+
+
+
+
+
+
+
+            
