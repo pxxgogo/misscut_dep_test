d5d14e450afb34c9980f716f08845c166d065008
pxxgogo <conviction.p@gmail.com>
2018-05-20 13:06:28 +0800

    debug

--- kernel/model/prob_model/prob_model.py
+++ kernel/model/prob_model/prob_model.py
@@ -156,7 +156,9 @@ class ProbModel:
                         continue
                     score = self.get_score((main_model_type, model_type_No + 3), dep_key, modified_word, words[1],
                                            words[2])
-                    print(dep_key, modified_word, words[1], words[2], score, words[0])
+                    if words[0] == "大家":
+                        print(closed_words)
+                        print(model_type_No + 3, analyzed_word_No, word, dep_key, modified_word, words[1], words[2], score, words[0])
                     if score > 0:
                         rets.append(self.generate_ret(model_type_No + 3, score, modified_word, words[1], words[2],
                                                       modified_flag=True))
