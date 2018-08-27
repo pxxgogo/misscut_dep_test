b0758847cfeaa193631ed29db4c7ffb3536c0519
pxxgogo <conviction.p@gmail.com>
2018-08-27 11:21:56 +0800

    update prob_model

--- kernel/model/prob_model/prob_model.py
+++ kernel/model/prob_model/prob_model.py
@@ -2,6 +2,8 @@ import plyvel
 import re
 import os
 import struct
+import json
+import redis
 
 NUMBER_RE_COMPILOR = re.compile(r"[\.-]?\d[0-9\.%-]*")
 NUMBER_TAG = "{{#}}"
@@ -9,6 +11,8 @@ NUMBER_TAG_LENGTH = len(NUMBER_TAG)
 LETTER_RE_COMPILOR = re.compile(r"[a-zA-Z][a-zA-Z\.'-]*")
 LETTER_TAG = "{{E}}"
 LETTER_TAG_LENGTH = len(LETTER_TAG)
+CONFIG_DIR = "/home/misscut/app/MissCut-V7/system/config.json"
+FAST_STAT_DB_FLAG = 2
 
 
 def replace_special_symbols(sentence):
@@ -26,11 +30,38 @@ def b_2_i(bytes):
     # print(bytes, struct.unpack(">q", bytes))
     return struct.unpack(">q", bytes)[0]
 
+def b_2_i_redis(bytes):
+    index = len(bytes) - 1
+    seed = 1
+    ret = 0
+    while index >= 0:
+        ret += seed * (bytes[index]-48)
+        seed *= 10
+        index -= 1
+    return ret
+
 
 class ProbModel:
-    def __init__(self, deep_model_dir="./model/leveldb/deep", broad_model_dir="./model/leveldb/broad",
-                 fre_model_dir="./model/leveldb/word_frequency/word_frequency.db",
-                 single_dep_model_dir="./model/leveldb/single_dep"):
+    instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls.instance:
+            return cls.instance
+        else:
+            cls.instance = cls()
+            return cls.instance
+
+    def __init__(self):
+        config_dir = CONFIG_DIR
+        with open(config_dir) as handle:
+            config = json.load(handle)
+        model_dirs = config["stat_models_dir"]
+        deep_model_dir = os.path.join(model_dirs["root_dir"], model_dirs["triples_deep_dir"])
+        broad_model_dir = os.path.join(model_dirs["root_dir"], model_dirs["triples_broad_dir"])
+        single_dep_model_dir = os.path.join(model_dirs["root_dir"], model_dirs["bigrams_dir"])
+        fre_model_dir = os.path.join(model_dirs["root_dir"], model_dirs["unigrams_dir"])
+
         self._deep_dbs = {'s1': plyvel.DB(os.path.join(deep_model_dir, 's1.db')),
                           's2': plyvel.DB(os.path.join(deep_model_dir, 's2.db')),
                           's3': plyvel.DB(os.path.join(deep_model_dir, 's3.db')),
@@ -51,21 +82,69 @@ class ProbModel:
                             'b12': plyvel.DB(os.path.join(single_dep_model_dir, 'b12.db'))}
         self._fre_db = plyvel.DB(fre_model_dir)
 
+        if FAST_STAT_DB_FLAG == 1:
+            self._redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+        elif FAST_STAT_DB_FLAG == 2:
+            sys.path.append(config["cpp_stat_model_dir"])
+            import prob_model
+            self._prob_cpp_db = prob_model.Prob_model_CPP()
+            self._prob_cpp_db.load_model(os.path.join(config["cpp_stat_model_dir"], config["cpp_stat_model_name"]))
         self._model_types = {0: "s1", 1: "s2", 2: "s3", 3: "b12", 4: "b13", 5: "b23", 6: "t123"}
-        self._single_dep_model_types = {0: "s1", 1: "s2", 2: "b12"}
+        self._single_dep_model_types = {0: "s-s1", 1: "s-s2", 2: "s-b12"}
+        # self._fast_db_timer = 0
+        # self._all_db_timer = 0
+        # self._fast_db_times = 0
+        # self._all_db_times = 0
 
-    def get_data(self, data):
-        rets = []
-        for sub_data in data:
-            ret = self.score(*sub_data)
-            rets.append(ret)
-        return rets
-
-    def score(self, type, word_1_info, label_1, word_2_info, label_2, word_3_info):
-        if type == 0:
-            dbs = self._deep_dbs
+    def _get_value(self, key, db_type):
+        fast_db_key = "%s %s" % (db_type, key)
+        # start_time = time.time()
+        if FAST_STAT_DB_FLAG == 1:
+            value_fast_db = self._redis_db.get(fast_db_key)
+        elif FAST_STAT_DB_FLAG == 2:
+            value_fast_db = self._prob_cpp_db.get(fast_db_key)
         else:
-            dbs = self._broad_dbs
+            value_fast_db = None
+        # end_time = time.time()
+        #
+        # self._fast_db_timer += end_time - start_time
+        # self._all_db_timer += end_time - start_time
+        # self._fast_db_times += 1
+        # self._all_db_times += 1
+
+        if value_fast_db is not None:
+            if FAST_STAT_DB_FLAG == 1:
+                return b_2_i_redis(value_fast_db)
+            elif FAST_STAT_DB_FLAG == 2:
+                if value_fast_db != 0:
+                    return value_fast_db
+        key = key.encode("utf-8")
+        # start_time = time.time()
+        if db_type == "fre":
+            value = self._fre_db.get(key)
+        else:
+            db_sub_type = db_type[2:]
+            db_main_type = db_type[0]
+            if db_main_type == 's':
+                value = self._single_dbs[db_sub_type].get(key)
+            elif db_main_type == 'd':
+                value = self._deep_dbs[db_sub_type].get(key)
+            elif db_main_type == 'b':
+                value = self._broad_dbs[db_sub_type].get(key)
+            else:
+                value = None
+        # end_time = time.time()
+        #
+        # self._all_db_timer += end_time - start_time
+        # self._all_db_times += 1
+
+        if value is None:
+            score = 0
+        else:
+            score = b_2_i(value)
+        return score
+
+    def _score(self, db_main_type_No, word_1_info, label_1, word_2_info, label_2, word_3_info):
         dep_key = "%s %s" % (label_1, label_2)
         word_1 = word_1_info["text"]
         word_2 = word_2_info["text"]
@@ -78,31 +157,16 @@ class ProbModel:
         modified_word_3 = replace_special_symbols(word_3)
 
         scores = []
-        key = modified_word_1.encode("utf-8")
-        value = self._fre_db.get(key)
-        if value is None:
-            score = 0
-        else:
-            score = b_2_i(value)
+        score = self._get_value(modified_word_1, "fre")
         scores.append(score)
 
-        key = modified_word_2.encode("utf-8")
-        value = self._fre_db.get(key)
-        if value is None:
-            score = 0
-        else:
-            score = b_2_i(value)
+        score = self._get_value(modified_word_2, "fre")
         scores.append(score)
 
-        key = modified_word_3.encode("utf-8")
-        value = self._fre_db.get(key)
-        if value is None:
-            score = 0
-        else:
-            score = b_2_i(value)
+        score = self._get_value(modified_word_3, "fre")
         scores.append(score)
 
-        if type == 0:
+        if db_main_type_No == 0:
             for model_type_No in range(6):
                 if model_type_No == 0:
                     key = "%s: %s" % (label_1, modified_word_1)
@@ -118,13 +182,8 @@ class ProbModel:
                     key = "%s: %s %s" % (label_2, modified_word_2, modified_word_3)
                 else:
                     key = ""
-                db = self._single_dbs[self._single_dep_model_types[model_type_No % 3]]
-                key = key.encode("utf-8")
-                value = db.get(key)
-                if value is None:
-                    score = 0
-                else:
-                    score = b_2_i(value)
+                db_type = self._single_dep_model_types[model_type_No % 3]
+                score = self._get_value(key, db_type)
                 scores.append(score)
         else:
             for model_type_No in range(6):
@@ -142,15 +201,9 @@ class ProbModel:
                     key = "%s: %s %s" % (label_2, modified_word_1, modified_word_3)
                 else:
                     key = ""
-                db = self._single_dbs[self._single_dep_model_types[model_type_No % 3]]
-                key = key.encode("utf-8")
-                value = db.get(key)
-                if value is None:
-                    score = 0
-                else:
-                    score = b_2_i(value)
+                db_type = self._single_dep_model_types[model_type_No % 3]
+                score = self._get_value(key, db_type)
                 scores.append(score)
-
 
         for model_type_No in range(7):
             if model_type_No == 0:
@@ -169,12 +222,34 @@ class ProbModel:
                 key = "%s: %s %s %s" % (dep_key, modified_word_1, modified_word_2, modified_word_3)
             else:
                 key = ""
-            db = dbs[self._model_types[model_type_No]]
-            key = key.encode("utf-8")
-            value = db.get(key)
-            if value is None:
-                score = 0
+            db_sub_type = self._model_types[model_type_No]
+            if db_main_type_No == 0:
+                db_type = "d-" + db_sub_type
             else:
-                score = b_2_i(value)
+                db_type = "b-" + db_sub_type
+            score = self._get_value(key, db_type)
             scores.append(score)
         return scores
+
+
+    # def init_timer(self):
+    #     self._fast_db_timer = self._all_db_timer = 0
+    #     self._fast_db_times = self._all_db_times = 0
+    #
+    #
+    # def get_timer(self):
+    #     return self._all_db_timer, self._fast_db_timer, self._all_db_times, self._fast_db_times
+
+    def get_data(self, data):
+        rets = []
+        for sub_data in data:
+            ret = self._score(*sub_data)
+            rets.append(ret)
+        return rets
+
+    def get_fre_value(self, word):
+        modified_word = replace_special_symbols(word)
+        score = self._get_value(modified_word, 'fre')
+        return score
+
+
